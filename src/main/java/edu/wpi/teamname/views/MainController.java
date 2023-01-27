a8e3c26dee4119748bf6050f41f97124a6e24229
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2023-01-27 15:29:45 -0500

    ECB works

--- src/main/java/edu/wpi/teamname/views/MainController.java
+++ src/main/java/edu/wpi/teamname/views/MainController.java
@@ -1,5 +1,6 @@
 package edu.wpi.teamname.views;
 
+import edu.wpi.teamname.entity.Mortgage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -10,17 +11,38 @@ public class MainController {
   @FXML private TextField yearField;
   @FXML private Button computeButton;
 
-  public MainController() {
+  private Mortgage mortgage;
+
+  public MainController(Mortgage mortgage) {
     /* Inject entity objects */
+    this.mortgage = mortgage;
   }
 
   /** Set the properties of the mortgage model and calculate the montly interest */
   @FXML
-  private void calculateTotalClicked() {}
+  private void calculateTotalClicked() {
+    double amount = Double.parseDouble(amountField.getText());
+    double rate = Double.parseDouble(rateField.getText());
+    int year = Integer.parseInt(yearField.getText());
+
+    this.mortgage.setAmount(amount);
+    this.mortgage.setRate(rate);
+    this.mortgage.setYears(year);
+
+    mortgage.setMonthlyPayment();
+  }
 
   /**
    * Enables the calculate button if every text box is filled. Disables it if any text box is empty
    */
   @FXML
-  private void validateButton() {}
+  private void validateButton() {
+
+    boolean valid =
+        !this.amountField.getText().equals("")
+            && !this.rateField.getText().equals("")
+            && !this.yearField.getText().equals("");
+
+    computeButton.setDisable(!valid);
+  }
 }
